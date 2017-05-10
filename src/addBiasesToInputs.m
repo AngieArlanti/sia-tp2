c24fe66b39515f0f526a896c0260096ba155d085
Agop Hurmuz <agophurmuz@gmail.com>
2017-05-09 21:22:40 -0300

    corrected addBiasesToInput and corrected train

--- src/addBiasesToInputs.m
+++ src/addBiasesToInputs.m
@@ -1,6 +1,6 @@
 function inputsWithBiases = addBiasesToInputs(inputs)
   inputsWithBiases = {};
   for i = 1:rows(inputs)
-    inputsWithBiases{i}{1} = [inputs(i,:) -1];
+    inputsWithBiases{i} = [inputs{i}(i,:) -1];
   endfor
 endfunction
