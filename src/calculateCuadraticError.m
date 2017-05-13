1797b40a3394ea98e34c94c9d671c1a7929d6576
Agop Hurmuz <agophurmuz@gmail.com>
2017-05-13 17:11:20 -0300

    add draw and fixes

--- src/calculateCuadraticError.m
+++ src/calculateCuadraticError.m
@@ -1,9 +1,10 @@
 function cuadraticError = calculateCuadraticError(patterns, updatedWeights, expectedOutputs)
-
+  patterns
+  inputsWithBiases = addBiasesToInputs(patterns);
   error = 0;
-  for u = 1:length(patterns)
-    obtainedOutput =  evaluateNetwork(patterns{u}, updatedWeights);
-    error += sqrt(expectedOutputs{u} - obtainedOutput);
+  for u = 1:length(inputsWithBiases)
+    obtainedOutput =  evaluateNetwork(inputsWithBiases{u}, updatedWeights);
+    error += sqrt(expectedOutputs{u} - obtainedOutput{1});
   endfor
 
   cuadraticError = error/2*length(patterns);
--- src/main.m
+++ src/main.m
@@ -2,12 +2,13 @@ function main()
 
   #One epoch is one train.
 
-  acceptedError = 10^-3;
+  acceptedError = 10^-1;
   cuadraticError = 1;
   cuadraticTestingError = 1;
-  learningPatterns = {[-1.8438,-0.9657,-1],[  0,-1.6551,-1],[-2.3054,1.6704,-1], [-0.5935,0,-1]};
-  expectedLearningOutputs={[0.0113],[0.2423],[0.1211],[0.3320]};
-  testingPatterns = {[-1.8438,1.6704,-1],[-1.6423,1.2835,-1],[3.1648,2.0107,-1], [-0.2894,1.3745,-1]};
+  learningPatterns = {[-1.8438,-0.9657],[  0,-1.6551]},[-2.3054,1.6704], [-0.5935,0]};
+  learningPatterns
+  expectedLearningOutputs={[0.0113],[0.2423]},[0.1211],[0.3320]};
+  testingPatterns = {[-1.8438,1.6704],[-1.6423,1.2835],[3.1648,2.0107], [-0.2894,1.3745]};
   expectedTestingOutputs={[0.3054],[0.6634],[0.0000],[0.5346]};
   weights = calculateWeights([2,2,1]); #Le pasamos la arquitectura.
   #The epoch corresponds with index.
@@ -17,10 +18,9 @@ function main()
 
   while(!learned(cuadraticError,acceptedError))
     #Training
-    updatedWeights = train(learningPatterns, weights, expectedLearningOutputs, etha);
+    [updatedWeights, obtainedOutputs] = train(learningPatterns, weights, expectedLearningOutputs, etha);
     cuadraticError = calculateCuadraticError(learningPatterns, updatedWeights, expectedLearningOutputs);
     learningErrors = [learningErrors,cuadraticError];
-
     #Testing
     cuadraticTestingError = calculateCuadraticError(testingPatterns, updatedWeights, expectedTestingOutputs);
     testingErrors = [testingErrors,cuadraticTestingError];
@@ -34,6 +34,7 @@ function main()
     weights = updatedWeights;
   end
 
-  plot(1:1:length(learningErrors), learningErrors,1:1:length(learningErrors),testingErrors);
+  #plot(1:1:length(learningErrors), learningErrors,1:1:length(learningErrors),testingErrors);
+  draw(learningPatterns, expectedLearningOutputs, obtainedOutputs);
 
 endfunction
