3dc929bb35d920f78ed071c22cde2007200790a2
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-05-15 21:51:15 -0300

    - Added 0.1

--- src/main.m
+++ src/main.m
@@ -7,13 +7,13 @@ function main()
   cuadraticError = 1;
   cuadraticTestingError = 1;
   maxEpochs = configuration.maxEpochs;
-  #learningPatterns = {[-1.8438,-0.9657],[  0,-1.6551]}#,[-2.3054,1.6704], [-0.5935,0]};
-  #expectedLearningOutputs={[0.0113],[0.2423]}#,[0.1211],[0.3320]};
+  learningPatterns = {[0.882,-0.9657],[ -0.5935,0.8697],[-0.8171 0.8697],[1.2987	-1.3099],[1.4695	-1.6551],[1.4695	-0.3034],[-1.8438	0.8697],[-1.8438	1.3745]};#[0.3224	-1.6093],[0.882	0],[-0.2894	1.6704],[0	-0.6209],[0	0.8697],[0.3224	-0.3034],[0.3224	0.2898],[-0.2894	-0.3034]};
+  expectedLearningOutputs={[0.9331],[0.9014],[0.9025],[0.9139],[0.5155],[0.5202],[0.437],[0.4383]};#,[0.3995],[0.4104],[0.4119],[0.3702],[0.3736],[0.4181],[0.2326],[0.2206]};
   #testingPatterns = {[-1.8438,1.6704],[-1.6423,1.2835],[3.1648,2.0107], [-0.2894,1.3745]};
   #expectedTestingOutputs={[0.3054],[0.6634],[0.0000],[0.5346]};
 
-  learningPatterns = configuration.learningDataInputs;
-  expectedLearningOutputs = configuration.learningDataExpectedOutputs;
+  #learningPatterns = configuration.learningDataInputs;
+  #expectedLearningOutputs = configuration.learningDataExpectedOutputs;
   testingPatterns = configuration.testingDataInputs;
   expectedTestingOutputs = configuration.testingDataExpectedOutputs;
   architecture = configuration.architecture;
